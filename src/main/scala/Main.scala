a04303a2e3c4888eeb8749836aa6d170fa41008d
linh nguyen <jonathanrocrach2@gmail.com>
2020-09-21 14:20:12 +0700

    make code cleaner

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,32 +1,34 @@
 import org.apache.spark.sql.SparkSession
 
-
+// TODO: replace
+case class Person(name: String,
+                  email: String,
+                  age: String,
+                  height: Double,
+                  university: String)
 
 object Main{
-  case class Person(name: String,
-                    email: String,
-                    age: String,
-                    height: Double)
-
   def main(args: Array [String]){
     val spark = SparkSession
       .builder()
       .appName("Spark with hbase")
       .getOrCreate()
 
-
-    val filepath = "file.json" //replace
+    // TODO: replace
+    val filepath = "file.json"
+    val tableName = "person"
+    val hbaseTableMapping = "name STRING :key, email STRING c:email, " +
+                            "age DATE p:age, height DOUBLE p:height, " +
+                            "university String school:university"
 
     import spark.implicits._
-    val df = spark.read.json(filepath).as[Person]
+    val df = spark.read.json(filepath).as[Person] // may use dataframe
 
     df.printSchema()
 
     df.write.format("org.apache.hadoop.hbase.spark")
-      .option("hbase.columns.mapping",
-        "name STRING :key, email STRING c:email, " +
-          "age DATE p:age, height FLOAT p:height")
-      .option("hbase.table", "person")
+      .option("hbase.table", tableName)
+      .option("hbase.columns.mapping", hbaseTableMapping)
       .option("hbase.spark.use.hbasecontext", false)
       .save()
 
