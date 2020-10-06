e73e511064c77614b1f687f1bb36383568927563
linhnln <jonathanrocrach2@gmail.com>
2020-10-06 15:33:36 +0700

    done one col

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,11 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.hadoop.hbase.spark.HBaseContext
 import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.spark.sql.functions.split
+import org.apache.spark.sql.functions._
+
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+
 
 object Main{
   def toInt(s: String): Int = util.Try(s.toInt).getOrElse(0)
@@ -16,46 +20,54 @@ object Main{
   val fileName = args(1)
   val configDf = spark.read.option("multiline", "true").json(fileName)
 
-   val conf = HBaseConfiguration.create()
-   conf.set("hbase.zookeeper.quorum", configDf.groupBy("QUORUM").mean().collect()(0)(0).toString)
-   conf.set("hbase.zookeeper.property.clientPort", configDf.groupBy("PORT").mean().collect()(0)(0).toString)
-   conf.set("hbase.rootdir","/apps/hbase/data")
-   conf.set("zookeeper.znode.parent","/hbase-unsecure")
-   conf.set("hbase.cluster.distributed","true")
+  val hd_conf = spark.sparkContext.hadoopConfiguration
+  val fs = FileSystem.get(hd_conf)
+  val dirPath = new Path(configDf.groupBy("FILE_PATH").mean().collect()(0)(0).toString)
+  var files = fs.listStatus(dirPath )
 
-   new HBaseContext(spark.sparkContext, conf)
-
-   var df = spark.read.parquet(configDf.groupBy("FILE_PATH").mean().collect()(0)(0).toString)
-
-    df = df.withColumn("tmp", split(df("_c0"), "\\|"))
-
-//    df = df.withColumn("col0", df("tmp").getItem(0))
-//          .withColumn("col1", df("tmp").getItem(1))
-//          .withColumn("col2", df("tmp").getItem(2))
-//          .withColumn("col3", df("tmp").getItem(3))
-//          .withColumn("col4", df("tmp").getItem(4))
-//          .withColumn("col5", df("tmp").getItem(5))
-//          .withColumn("col6", df("tmp").getItem(6))
-//          .withColumn("col7", df("tmp").getItem(7))
-
-    val num_of_columns : Int = toInt(configDf.groupBy("NUM_OF_COLUMN").mean().collect()(0)(0).toString)
-
-    for (i <- 0 until num_of_columns ){
-      df = df.withColumn("col".concat(i.toString), df("tmp").getItem(i))
+  files = files.sortWith((s, t) => {
+     s.getPath.toString.compareTo(t.getPath.toString) > 0
     }
+  )
 
-    df = df.drop(df("_c0"))
-    df = df.drop(df("tmp"))
-    df = df.drop(df("FileName"))
+  val latest_file = files(0).getPath.toString
+  val near_latest_file = files(1).getPath.toString
 
-    df.printSchema()
+  val conf = HBaseConfiguration.create()
+  conf.set("hbase.zookeeper.quorum", configDf.groupBy("QUORUM").mean().collect()(0)(0).toString)
+  conf.set("hbase.zookeeper.property.clientPort", configDf.groupBy("PORT").mean().collect()(0)(0).toString)
+  conf.set("hbase.rootdir","/apps/hbase/data")
+  conf.set("zookeeper.znode.parent","/hbase-unsecure")
+  conf.set("hbase.cluster.distributed","true")
 
-    df.show(30)
+  new HBaseContext(spark.sparkContext, conf)
 
-   df.write.format("org.apache.hadoop.hbase.spark")
-     .option("hbase.table", configDf.groupBy("TABLE_NAME").mean().collect()(0)(0).toString)
-     .option("hbase.columns.mapping", configDf.groupBy("TABLE_SCHEMA").mean().collect()(0)(0).toString)
-     .save()
+  var df = spark.read.parquet(near_latest_file)
+
+  df = df.select("_c0","_c1", "_c2")
+  df = df.withColumnRenamed("_c0", "old_id")
+  df = df.withColumnRenamed("_c1", "BRAND_OLD")
+  df = df.withColumnRenamed("_c2", "MODEL_OLD")
+
+  var df_1 = spark.read.parquet(latest_file)
+
+  df_1 = df_1.select("_c0","_c1", "_c2")
+  df_1 = df_1.withColumnRenamed("_c0", "new_id")
+  df_1 = df_1.withColumnRenamed("_c1", "BRAND_NEW")
+  df_1 = df_1.withColumnRenamed("_c2", "MODEL_NEW")
+
+  df_1 = df_1.join(df, df("old_id") === df_1("new_id"))
+  df_1 = df_1.withColumn("CHANGED_PHONE", expr("MODEL_NEW != MODEL_OLD"))
+
+  df_1.printSchema()
+
+  df_1 = df_1.drop("old_id")
+  df_1.show(10)
+
+  df_1.write.format("org.apache.hadoop.hbase.spark")
+    .option("hbase.table", configDf.groupBy("TABLE_NAME").mean().collect()(0)(0).toString)
+    .option("hbase.columns.mapping", configDf.groupBy("TABLE_SCHEMA").mean().collect()(0)(0).toString)
+    .save()
 
    println("Done")
   }
