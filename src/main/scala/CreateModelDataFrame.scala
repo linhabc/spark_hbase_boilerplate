20851812bd3fda37609446c52c4005ea67c0687a
linh.nln <jonathanrocrach2@gmail.com>
2022-07-07 09:53:22 +0700

    update

--- src/main/scala/CreateModelDataFrame.scala
+++ src/main/scala/CreateModelDataFrame.scala
@@ -51,76 +51,90 @@ object CreateModelDataFrame{
       for (file <- files){
         val month = toInt(file.getPath.toString.slice(85, 85 + 2))
         val year = toInt(file.getPath.toString.slice(81, 81 + 4))
+        println("init month ", month)
+        println("init year ", year)
 
-          if( month == tmpMonth  && year == YEAR) {
-            println("startMonth "+startMonth)
+        var debitMonth = month + 1
+        var debitYear = year
+        if(debitMonth > 12){
+          debitMonth = debitMonth - 12;
+          debitYear = debitYear + 1;
+        }
 
-            println("month ", month)
-            println("year ", year)
-            println(file.getPath.toString)
+        println("init debitMonth ", debitMonth, tmpMonth)
+        println("init debitYear ", debitYear, YEAR)
 
-            var df_debit = spark.read.parquet(file.getPath.toString)
+        if( debitMonth == tmpMonth  && debitYear == YEAR) {
+//          println("startMonth "+startMonth)
+//
+//          println("month ", month)
+//          println("year ", year)
+//          println("debitMonth ", debitMonth)
+//          println("debitYear ", debitYear)
+          println(file.getPath.toString)
 
-            df_debit = df_debit.drop(df_debit("_c0"))
+          var df_debit = spark.read.parquet(file.getPath.toString)
 
-            df_debit.count()
+          df_debit = df_debit.drop(df_debit("_c0"))
+
+          df_debit.count()
 //            df_debit.show(false)
 
-            // read using packet dataframe
-            val packet_df = spark.read.parquet("/user/MobiScore_Output/post_payment/mobile_internet/mobile_internet_dataframe("+month+")("+YEAR+").parquet")
+          // read using packet dataframe
+          val packet_df = spark.read.parquet("/user/MobiScore_Output/post_payment/mobile_internet/mobile_internet_dataframe("+tmpMonth+")("+YEAR+").parquet")
 
-            packet_df.count()
-            packet_df.show(false)
+          packet_df.count()
+          packet_df.show(false)
 
-            df_debit.createOrReplaceTempView("table")
-            var result = spark.sql("select distinct _c1, _c3, _c5, _c6 from table")
+          df_debit.createOrReplaceTempView("table")
+          var result = spark.sql("select distinct _c1, _c3, _c5, _c6 from table")
 
-            result = result.na.fill(0)
-            result = result.na.fill("0")
-
-//            result.show(false)
-
-            result.createOrReplaceTempView("table")
-            result = spark.sql("select _c1, sum(_c3) as SUM_USE, sum(_c5) as SUM_PAY, max(_c6) as USING_BANK_tmp from table group by _c1")
-//            result.show(false)
-
-            // filter outlier then calculate avg value
-            val result_filter = result.filter(result("SUM_USE") < 4000000 || result("SUM_PAY") < 4000000)
-            val avg_using = result_filter.agg(expr("avg(SUM_USE)").as("AVG_USE")).select(col("AVG_USE")).collect()(0)(0)
-            val avg_paying = result_filter.agg(expr("avg(SUM_PAY)").as("AVG_PAY")).select(col("AVG_PAY")).collect()(0)(0)
-
-            // join with packet_df
-            result = result.join(packet_df, result("_c1") === packet_df("col0"), "left")
-            result = result.drop("col0")
-
-            result = result.na.fill(0)
-            result = result.na.fill("0")
-
-            // determine feature value
-            result = result.withColumn("PAY_IN_TIME" , result("SUM_USE") <= result("SUM_PAY"))
-            result = result.withColumn("SPARE_PAYMENT" , result("SUM_USE") < result("SUM_PAY"))
-            result = result.withColumn("HIGHER_THAN_AVG_USING", result("SUM_USE") >= avg_using)
-            result = result.withColumn("HIGHER_THAN_AVG_PAYING", result("SUM_PAY") >= avg_paying)
-            result = result.withColumn("USING_BANK", result("USING_BANK_tmp") === 2)
-            result = result.withColumn("NOT_USING_PACKET", result("HIGHER_THAN_AVG_PAYING") && result("packet_"+month) === 0)
+          result = result.na.fill(0)
+          result = result.na.fill("0")
 
 //            result.show(false)
 
-            //calculate payment history score
-            result = result.withColumn("sc_1_"+ month, when(col("PAY_IN_TIME") === true, 1).otherwise(0))
-              .withColumn("sc_2_"+ month, when(col("SPARE_PAYMENT") === true, 1).otherwise(0))
-              .withColumn("sc_3_"+ month, when(col("HIGHER_THAN_AVG_USING") === true && col("PAY_IN_TIME") === true, 1).otherwise(0))
-              .withColumn("sc_4_"+ month, when(col("HIGHER_THAN_AVG_PAYING") === true, 1).otherwise(0))
-              .withColumn("sc_5_"+ month, when(col("USING_BANK") === true, 1).otherwise(0))
-              .withColumn("sc_6_"+ month, when(col("NOT_USING_PACKET") === true, 1).otherwise(0))
+          result.createOrReplaceTempView("table")
+          result = spark.sql("select _c1, sum(_c3) as SUM_USE, sum(_c5) as SUM_PAY, max(_c6) as USING_BANK_tmp from table group by _c1")
+//            result.show(false)
 
-            result = result.select("_c1", "sc_1_" + month, "sc_2_"+ month, "sc_3_"+ month, "sc_4_"+ month, "sc_5_"+ month, "sc_6_"+ month)
-            //      result = result.select("_c1", "sc_1_" + month, "sc_2_"+ month, "sc_3_"+ month, "sc_4_"+ month, "sc_5_"+ month)
+          // filter outlier then calculate avg value
+          val result_filter = result.filter(result("SUM_USE") < 4000000 || result("SUM_PAY") < 4000000)
+          val avg_using = result_filter.agg(expr("avg(SUM_USE)").as("AVG_USE")).select(col("AVG_USE")).collect()(0)(0)
+          val avg_paying = result_filter.agg(expr("avg(SUM_PAY)").as("AVG_PAY")).select(col("AVG_PAY")).collect()(0)(0)
 
-            result.show(false)
+          // join with packet_df
+          result = result.join(packet_df, result("_c1") === packet_df("col0"), "left")
+          result = result.drop("col0")
 
-            score = score.join(result, score("col0") === result("_c1"), "left")
-          }
+          result = result.na.fill(0)
+          result = result.na.fill("0")
+
+          // determine feature value
+          result = result.withColumn("PAY_IN_TIME" , result("SUM_USE") <= result("SUM_PAY"))
+          result = result.withColumn("SPARE_PAYMENT" , result("SUM_USE") < result("SUM_PAY"))
+          result = result.withColumn("HIGHER_THAN_AVG_USING", result("SUM_USE") >= avg_using)
+          result = result.withColumn("HIGHER_THAN_AVG_PAYING", result("SUM_PAY") >= avg_paying)
+          result = result.withColumn("USING_BANK", result("USING_BANK_tmp") === 2)
+          result = result.withColumn("NOT_USING_PACKET", result("HIGHER_THAN_AVG_PAYING") && result("packet_"+tmpMonth) === 0)
+
+//            result.show(false)
+
+          //calculate payment history score
+          result = result.withColumn("sc_1_"+ tmpMonth, when(col("PAY_IN_TIME") === true, 1).otherwise(0))
+            .withColumn("sc_2_"+ tmpMonth, when(col("SPARE_PAYMENT") === true, 1).otherwise(0))
+            .withColumn("sc_3_"+ tmpMonth, when(col("HIGHER_THAN_AVG_USING") === true && col("PAY_IN_TIME") === true, 1).otherwise(0))
+            .withColumn("sc_4_"+ tmpMonth, when(col("HIGHER_THAN_AVG_PAYING") === true, 1).otherwise(0))
+            .withColumn("sc_5_"+ tmpMonth, when(col("USING_BANK") === true, 1).otherwise(0))
+            .withColumn("sc_6_"+ tmpMonth, when(col("NOT_USING_PACKET") === true, 1).otherwise(0))
+
+          result = result.select("_c1", "sc_1_" + tmpMonth, "sc_2_"+ tmpMonth, "sc_3_"+ tmpMonth, "sc_4_"+ tmpMonth, "sc_5_"+ tmpMonth, "sc_6_"+ tmpMonth)
+          //      result = result.select("_c1", "sc_1_" + month, "sc_2_"+ month, "sc_3_"+ month, "sc_4_"+ month, "sc_5_"+ month)
+
+          result.show(false)
+
+          score = score.join(result, score("col0") === result("_c1"), "left")
+        }
       }
 
     }
